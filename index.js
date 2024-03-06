efa093aa9bcfe2de3862123f050324aa6d391570
cDeshalPadmakar <cdeshalpadmakar@gmail.com>
2024-03-06 17:15:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://temp-8a755-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "https://add-to-cart-bdab4-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
